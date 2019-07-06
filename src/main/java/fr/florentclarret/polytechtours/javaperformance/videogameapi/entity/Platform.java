0f7b4debf4bcd7b200ba3c208f7281a6c030ff19
Florent Clarret <florent.clarret@gmail.com>
2019-07-06 09:16:20 +0200

    refactor(db): remove unused oneToMany relationships

--- src/main/java/fr/florentclarret/polytechtours/javaperformance/videogameapi/entity/Platform.java
+++ src/main/java/fr/florentclarret/polytechtours/javaperformance/videogameapi/entity/Platform.java
@@ -1,28 +1,10 @@
 package fr.florentclarret.polytechtours.javaperformance.videogameapi.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import io.swagger.annotations.ApiModelProperty;
-
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import java.util.List;
 
 @Entity
 @Table(name = "platform")
 public final class Platform extends BaseEntity {
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "platform")
-    @JsonIgnore
-    @ApiModelProperty(hidden = true)
-    private List<VideoGame> videoGameList;
-
-    public List<VideoGame> getVideoGameList() {
-        return videoGameList;
-    }
-
-    public void setVideoGameList(final List<VideoGame> videoGameList) {
-        this.videoGameList = videoGameList;
-    }
 }
--- src/main/java/fr/florentclarret/polytechtours/javaperformance/videogameapi/entity/Publisher.java
+++ src/main/java/fr/florentclarret/polytechtours/javaperformance/videogameapi/entity/Publisher.java
@@ -1,29 +1,10 @@
 package fr.florentclarret.polytechtours.javaperformance.videogameapi.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import io.swagger.annotations.ApiModelProperty;
-
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "publisher")
 public final class Publisher extends BaseEntity {
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "publisher")
-    @ApiModelProperty(hidden = true)
-    @JsonIgnore
-    private List<VideoGame> videoGameList = new ArrayList<>();
-
-    public List<VideoGame> getVideoGameList() {
-        return videoGameList;
-    }
-
-    public void setVideoGameList(final List<VideoGame> videoGameList) {
-        this.videoGameList = videoGameList;
-    }
 }
